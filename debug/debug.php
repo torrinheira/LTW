b8bdc68551d3944ec2a3bcc66076f6c53f0396dd
Bernardo Santos <bernardosantos670@gmail.com>
2019-11-25 14:39:49 +0000

    Implemented a function to print a variable value to the console

---
+++ debug/debug.php
@@ -0,0 +1,11 @@
+<?php
+
+function console_log($data) {
+    $output = $data;
+    if (is_array($output)) {
+        $output = implode(', ', $output);
+    }
+
+    echo '<script> console.log(\'Debug : ' . $output . '\'); </script>';
+}
+?>
