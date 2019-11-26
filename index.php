d351ae5faf6693ce0090b58ecd80b67717d1bd51
Bernardo Santos <bernardosantos670@gmail.com>
2019-11-26 18:10:29 +0000

    Formated main page code

--- index.php
+++ index.php
@@ -11,14 +11,12 @@
         <title>Place Genie</title>
         <meta charset="UTF-8">
 
-        <script src="/javascript/valid-date.js" async></script>
+        <script src="./javascript/valid-date.js" async></script>
     </head>
     <body>
 
         <?php draw_header(); ?>
-
         <?php draw_search(); ?>
-
         <?php draw_footer(); ?>
 
     </body>
