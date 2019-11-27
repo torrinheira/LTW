c34c46cacdcc9176769cbdf33d7e51b7524b3a90
Vitor Hugo <vhlg99@gmail.com>
2019-11-27 12:21:05 +0000

    starting to work with properties

--- database/place-genie.sql
+++ database/place-genie.sql
@@ -10,24 +10,29 @@ CREATE TABLE user (
 );
 
 CREATE TABLE property (
-    id                    INTEGER PRIMARY KEY,
+    id                    INTEGER PRIMARY KEY AUTOINCREMENT,
     title                 VARCHAR,
     price                 REAL NOT NULL ON CONFLICT ABORT,
-    city_id               INTEGER REFERENCES city(id),
+    city                  VARCHAR NOT NULL ON CONFLICT ABORT,  
+    address               VARCHAR NOT NULL ON CONFLICT ABORT,            
     description           VARCHAR,
     capacity              INTEGER,
-    owner_username        INTEGER REFERENCES user(username) ON DELETE CASCADE
+    owner_username        VARCHAR REFERENCES user(username) ON DELETE CASCADE
 );
 
+
+CREATE TABLE reservation (
+    id                    INTEGER PRIMARY KEY AUTOINCREMENT,
+    property_id           INTEGER REFERENCES property(id),
+    tourist_username      VARCHAR REFERENCES user(username) ON DELETE CASCADE,
+    arrival_date          DATE NOT NULL ON CONFLICT ABORT,
+    departure_date        DATE NOT NULL ON CONFLICT ABORT
+);
+
+
+/*
 CREATE TABLE city (
     id              INTEGER PRIMARY KEY,
     name            VARCHAR NOT NULL UNIQUE
 );
-
-CREATE TABLE reservation (
-    id                    INTEGER PRIMARY KEY,
-    property_id           INTEGER REFERENCES property(id),
-    tourist_username      INTEGER REFERENCES user(username) ON DELETE CASCADE,
-    arrival_date          DATE NOT NULL ON CONFLICT ABORT,
-    departure_date        DATE NOT NULL ON CONFLICT ABORT
-);
+*/
