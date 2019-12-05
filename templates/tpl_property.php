2ce6e2a54b623c0e267a3fb01971b13eac6f8d33
Vitor Hugo <vhlg99@gmail.com>
2019-12-05 17:22:01 +0000

    Started reservation :)

---
+++ templates/tpl_property.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+    function draw_reservation() {?>
+        <section id="reservation_form">
+            <form>
+                
+            </form>
+        </section>  
+    <?php
+    }
+?>
