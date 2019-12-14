690dd18cf8947e68da596cf051fcd488139d87d0
Vitor Hugo <vhlg99@gmail.com>
2019-12-14 11:50:19 +0000

    working on manage properties

--- pages/manage_properties.php
+++ pages/manage_properties.php
@@ -28,6 +28,8 @@ $number_of_properties = count($user_properties);
     <meta charset="utf-8">
 
     <link href="../css/common.css" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
     <script src="../javascript/messages.js" type="module" defer></script>
 
 </head>
--- pages/manage_reservations.php
+++ pages/manage_reservations.php
@@ -35,6 +35,8 @@ $number_current = count($current_reservation);
     <meta charset="utf-8">
 
     <link href="../css/common.css" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
     <script src="../javascript/messages.js" type="module" defer></script>
 
 </head>
